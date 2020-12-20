5dc68c3e3c723f0f61eaf568de60b8d60ec1dee0
Yesh Chandiramani <yesh0907@hotmail.com>
2020-12-20 15:13:12 +0800

    Add Argon Design System

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import DemoNavbar from "components/DemoNavbar";
+import { Container, Row, Col, Button } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <DemoNavbar />
+      <main>
+        <div className="position-relative">
+          {/* shape Hero */}
+          <section className="section section-lg section-shaped pb-250">
+            <div className="shape shape-style-1 shape-default">
+            </div>
+            <Container className="py-lg-md d-flex">
+              <div className="col px-0">
+                <Row>
+                  <Col lg="6">
+                    <h1 className="display-3 text-white">
+                      A beautiful Design System{" "}
+                      <span>completed with examples</span>
+                    </h1>
+                    <p className="lead text-white">
+                      The design system comes with four pre-built pages to help
+                      you get started faster. You can change the text and images
+                      and you're good to go.
+                    </p>
+                    <div className="btn-wrapper">
+                      <Button
+                        className="btn-icon mb-3 mb-sm-0"
+                        color="info"
+                        href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/alerts?ref=adsr-landing-page"
+                      >
+                        <span className="btn-inner--icon mr-1">
+                          <i className="fa fa-code" />
+                        </span>
+                        <span className="btn-inner--text">Components</span>
+                      </Button>
+                      <Button
+                        className="btn-white btn-icon mb-3 mb-sm-0 ml-1"
+                        color="default"
+                        href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-landing-page"
+                      >
+                        <span className="btn-inner--icon mr-1">
+                          <i className="ni ni-cloud-download-95" />
+                        </span>
+                        <span className="btn-inner--text">Download React</span>
+                      </Button>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            </Container>
+            {/* SVG separator */}
+            <div className="separator separator-bottom separator-skew">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="none"
+                version="1.1"
+                viewBox="0 0 2560 100"
+                x="0"
+                y="0"
+              >
+                <polygon
+                  className="fill-white"
+                  points="2560 0 2560 100 0 100"
+                />
+              </svg>
+            </div>
+          </section>
+          {/* 1st Hero Variation */}
+        </div>
+      </main>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/font-awesome/css/font-awesome.min.css";
+import "assets/scss/argon-design-system-react.scss?v1.1.0";
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact render={props => <App {...props} />} />
+      <Redirect to="/" />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
